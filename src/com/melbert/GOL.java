b5296f3f2e604e6b3e0338d3de9da427f135ed7a
albertachren <albert.achren@gmail.com>
2019-05-15 13:31:19 +0200

    Merge branch 'master' of C:\Users\albert.achren\Documents\GitHub\tetris with conflicts.

--- src/com/melbert/GOL.java
+++ src/com/melbert/GOL.java
@@ -4,42 +4,57 @@ package com.melbert;
 import javax.swing.*;
 
 public class GOL {
-    public static int golUpate;
+    private static int golUpate = 1000;
+    private int[][] array;
 
-    public GOL(TetrisArray array) {
+    public GOL(TetrisArray tetrisArray) {
+        this.array = tetrisArray.getData();
         Timer timer = new Timer(golUpate, e -> {
-            update(array);
+            update(this.array);
+            System.out.println("update");
         });
-
+        timer.start();
 
     }
 
-    private void update(TetrisArray array) {
-        Tetris.tetris.tetrisPanel.setGraphics(updateArray(array));
+    private void update(int[][] array) {
+        this.array = updateArray(array);
+        Tetris.tetris.tetrisPanel.setGraphics(array);
     }
 
-    private int[][] updateArray(TetrisArray array) {
-        int[][] newArray = {{}};
-        int[][] data = array.getData();
+    private int[][] updateArray(int[][] array) {
+        int[][] data = array;
+        int[][] newArray = new int[data.length][data.length];
 
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data[0].length - 1; j++) {
                 newArray[i][j] = 0;
             }
         }
 
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data[0].length - 1; j++) {
                 try {
-                    if(data[i][j] == 0 || data[i][j] == 2){
+                    if (data[i][j] == 0 || data[i][j] == 2) {
 
-                        int nCount = countNeighbors(i,j,data);
+                        int nCount = countNeighbors(i, j, data);
+                        if (nCount == 3) {
+                            newArray[i][j] = 1;
+                        }
 
-
-
+                    } else if (data[i][j] == 1) {
+                        int nCount = countNeighbors(i, j, data);
+                        if (nCount < 2 || nCount > 3) {
+                            newArray[i][j] = 0;
+                        } else {
+                            newArray[i][j] = 1;
+                        }
                     }
-                    else if(data[i][j] == 1){}
-                } catch (Exception ignored) {
+                } catch (Exception e) {
+                    System.out.println("error golupdate");
+                    System.out.println(e.toString());
+                    System.out.println(e.getMessage().toString());
+                    System.out.println(e.getStackTrace()[0].toString());
                 }
                 ;
             }
@@ -49,14 +64,34 @@ public class GOL {
 
     private int countNeighbors(int i, int j, int[][] data) {
         int nCount = 0;
-        if(data[i-1][j-1] == 1){nCount++;}
-        if(data[i-1][j] == 1){nCount++;}
-        if(data[i-1][j+1] == 1){nCount++;}
-        if(data[i][j-1] == 1){nCount++;}
-        if(data[i][j+1] == 1){nCount++;}
-        if(data[i+1][j-1] == 1){nCount++;}
-        if(data[i+1][j] == 1){nCount++;}
-        if(data[i+1][j+1] == 1){nCount++;}
+        try {
+
+            if (data[i - 1][j - 1] == 1) {
+                nCount++;
+            }
+            if (data[i - 1][j] == 1) {
+                nCount++;
+            }
+            if (data[i - 1][j + 1] == 1) {
+                nCount++;
+            }
+            if (data[i][j - 1] == 1) {
+                nCount++;
+            }
+            if (data[i][j + 1] == 1) {
+                nCount++;
+            }
+            if (data[i + 1][j - 1] == 1) {
+                nCount++;
+            }
+            if (data[i + 1][j] == 1) {
+                nCount++;
+            }
+            if (data[i + 1][j + 1] == 1) {
+                nCount++;
+            }
+        } catch (Exception ignored) {
+        }
         return nCount;
     }
 }
